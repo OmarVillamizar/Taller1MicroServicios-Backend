61b9eac67c1f3d4a2cb371376f6a0a094106a232
Omar Villamizar <omarjesusvi@ufps.edu.co>
2025-09-02 16:21:23 -0500

    fix: all cors allowed

--- taller1/src/main/java/com/example/taller1/WebConfig.java
+++ taller1/src/main/java/com/example/taller1/WebConfig.java
@@ -6,14 +6,13 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer{
-	@Override
-
+public class WebConfig implements WebMvcConfigurer {
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
-
-        registry.addMapping("/**");
-
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
